954c30538353128e6f122383a45cf3122cfa739a
KangJaeHeon <gjsdms1244@gmail.com>
2025-07-30 20:26:02 +0900

    feat : 심화 문제(실버): 2941문제, 크로아티아 언어 확인 후 숫자 세기.

---
+++ silver/deepen/Main2941.java
@@ -0,0 +1,24 @@
+package silver.deepen;
+
+import java.io.*;
+
+public class Main2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] cro = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+
+        int count = 0;
+        String str = br.readLine();
+        for(String c :cro){
+            str = str.replace(c,"@");
+            
+        }
+        int num = str.length();
+        bw.write(String.valueOf(num));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
