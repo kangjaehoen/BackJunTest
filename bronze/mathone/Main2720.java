96f844a259f1a2511c46e43158be5ab24b08f8b1
KangJaeHeon <gjsdms1244@gmail.com>
2025-08-10 18:45:53 +0900

    feat : 수학1 문제(브론즈): 2720, 거스름돈 문제

---
+++ bronze/mathone/Main2720.java
@@ -0,0 +1,36 @@
+package bronze.mathone;
+
+import java.io.*;
+
+public class Main2720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str =  br.readLine();
+        int t = Integer.parseInt(str);
+
+        for(int i=0;i<t;i++){
+           String changeStr =  br.readLine();
+           int change = Integer.parseInt(changeStr);
+
+           int quarter = change / 25;
+           int remain1 = change - (quarter*25);
+
+           int dime = remain1/10;
+           int remain2 = remain1-(dime*10);
+
+           int nickel = remain2/5;
+           int remain3 = remain2-(nickel*5);
+
+           int penny = remain3/1;
+
+           bw.write(quarter+ " "+dime+" "+nickel+" "+penny);
+           bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
