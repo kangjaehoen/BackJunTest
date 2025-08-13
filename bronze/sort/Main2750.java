42bd745a8d8e8f5514a667f4b82e29e0c1b79b60
kang <gjsdms1244@gmail.com>
2025-08-13 22:04:58 +0900

    feat : 백준 2750 문제) 2750 문제 브론즈 오름차순 문제

---
+++ bronze/sort/Main2750.java
@@ -0,0 +1,32 @@
+package bronze.sort;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Arrays;
+
+public class Main2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n  = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+           int num =  Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+
+
+        Arrays.sort(arr);
+        for(int arrASC : arr){
+            bw.write(String.valueOf(arrASC));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
