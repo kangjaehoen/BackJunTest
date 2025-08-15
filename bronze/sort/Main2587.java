f4c1c7332250b19054d80f7f2c038579e2e57f11
kang <gjsdms1244@gmail.com>
2025-08-15 22:45:42 +0900

    feat : 백준 2587 문제) 2587 문제 브론즈 오름차순 문제

---
+++ bronze/sort/Main2587.java
@@ -0,0 +1,30 @@
+package bronze.sort;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> list = new ArrayList<>();
+
+        String str;
+        while((str = br.readLine()) !=null && br.readLine().isEmpty()) {
+            list.add(Integer.parseInt(str));
+        }
+
+        int sum =0;
+        for(int i=0;i<list.size();i++){
+            sum += list.get(i);
+        }
+        int avg = sum/list.size();
+
+        bw.write(String.valueOf(avg));
+
+        bw.flush();
+
+    }
+}
